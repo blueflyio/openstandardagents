d7b3a5096b53164c7c9545d1bfb39af293007442
bluefly <thomas.scola@bluefly.io>
2025-10-14 13:39:48 -0400

    feat: Production Architecture Cleanup - Shell/Makefile Elimination + OpenAPI Standardization

--- libs/src/protocols/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * OSSA Protocols Module
- *
- * Exports protocol implementations for OSSA specification
- */
-
-export * from './acap.js';
-export * from './rasp.js';
--- libs/src/server/middleware/asyncHandler.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Async handler middleware to catch async errors
- */
-
-import { Request, Response, NextFunction } from 'express';
-
-export const asyncHandler = (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
