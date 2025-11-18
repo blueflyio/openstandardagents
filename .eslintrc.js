308c75d2c6e9dc67c229a85718a88f7e5d62ef03
semantic-release <semantic-release@localhost>
2025-11-18 12:02:03 -0500

    fix: add ESLint configuration for TypeScript
    - Fixes 'ESLint couldn't find a configuration file' error
    - Configured for TypeScript with @typescript-eslint/parser
    - Ignores website/, examples/, and dist/ directories
    - Resolves release-all-platforms job failure

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2020: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'out/',
+    '*.js',
+    'website/',
+    'examples/',
+  ],
+};
