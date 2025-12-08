574f5a6f4bd9a026be98b76f21839b896ca93615
Thomas @ Bluefly.io <thomas.scola@bluefly.io>
2025-12-08 08:41:30 -0500

    fix(ci): sync milestone-workflow.yml to allow release/* targets

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,11 +10,6 @@ export * from './task';
 export type { OssaTask, TaskSpec, RuntimeBinding } from './task';
 export { isOssaTask, createTaskManifest } from './task';
 
-// Export Workflow types (v0.3.0)
-export * from './workflow';
-export type { OssaWorkflow, WorkflowSpec, WorkflowStep } from './workflow';
-export { isOssaWorkflow, createWorkflowManifest, createStep, expr } from './workflow';
-
 /**
  * Capability definition (OpenAPI-style operation)
  */
